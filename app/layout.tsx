49e4b2818dc78a8c7a5a5bb28faa58920f1c8672
Dan Stroot <dan@thestroots.com>
2023-01-06 16:38:16 -0800

    working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,27 @@
-import './globals.css'
+import "../styles/globals.css";
+// import Script from "next/script";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      {/* <Script id="dark-mode" strategy="beforeInteractive">
+        {`if (localStorage.theme === 'dark' || (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)){" "}
+        {document.documentElement.classList.add("dark")} else{" "}
+        {document.documentElement.classList.remove("dark")}`}
+      </Script> */}
+      <body className="px-16 transition duration-500 bg-white lg:px-24 dark:bg-gray-900">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
