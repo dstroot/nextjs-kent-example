9039fbd64cf226e770de2e590c8ecb182bce07c2
Dan Stroot <dan@thestroots.com>
2023-01-19 12:57:27 -0800

    Create Motion.ts

---
+++ components/Motion.ts
@@ -0,0 +1,11 @@
+"use client";
+
+/* 
+
+  You can wrap third-party components that rely on client-only features in your own Client Components
+
+*/
+
+import { LazyMotion, domAnimation, m } from "framer-motion";
+
+export LazyMotion, domAnimation, m;
