4361364241d9c7008fa8e1f0d6361dcafbcf0d26
Dan Stroot <dan@thestroots.com>
2023-01-19 14:54:43 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-"use client";
+// "use client";
 
 import Image from "next/image";
-// import m from "../components/Motion";
-import { m } from "framer-motion"
+import { m } from "../components/Motion";
+// import { m } from "framer-motion"
 
 // images
 import hero from "../public/img/kody-flying_red.webp";
