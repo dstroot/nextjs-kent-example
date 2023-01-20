3de093e985e6e21e464b78ec83a22698569fd535
Dan Stroot <dan@thestroots.com>
2023-01-19 16:05:12 -0800

    Update Providers.tsx

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-// import { LazyMotion, domAnimation } from "framer-motion"
+import { LazyMotion, domAnimation } from "framer-motion"
 
 // see: https://beta.nextjs.org/docs/rendering/server-and-client-components
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider attribute="class">
+      <LazyMotion features={domAnimation}>
         {children}
+      </LazyMotion>
     </ThemeProvider>
   );
 };
